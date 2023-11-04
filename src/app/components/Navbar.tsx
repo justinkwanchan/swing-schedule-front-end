8b049bbdaed1ca7682c7d67fdb7cf8a96b4ab74e
Justin <justinkwanchan@gmail.com>
2023-11-04 10:47:53 -0400

    Add formatting and placeholders

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,18 +1,27 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav>
-      {/* Montreal icon */}
-      <div>
+    <nav className="bg-teal-700 mx-auto p-4 flex justify-between items-center">
+      <div className="flex items-center gap-4">
+        <button className="text-3xl md:hidden focus:outline-none">
+          &#9776;
+        </button>
+        <div className="bg-gray-300 w-4 h-4">{/* Montreal icon */}</div>
+      </div>
+      <div className="hidden md:flex justify-between md:w-2/3 lg:w-1/2">
         <Link href="/">HOME</Link>
         <Link href="/events">ALL EVENTS</Link>
         <Link href="/contribute">HOW TO CONTRIBUTE</Link>
         <Link href="/large-events">LARGE EVENTS</Link>
       </div>
-      {/* Instagram icon */}
-      <Link href="/login">LOGIN</Link>
-      {/* En/Fr switch */}
+      <div className="flex items-center gap-4">
+        <div className="bg-gray-300 hidden md:block w-4 h-4">
+          {/* Instagram icon */}
+        </div>
+        <Link href="/login">LOGIN</Link>
+        <div className="bg-gray-300 w-4 h-4">{/* En/Fr switch */}</div>
+      </div>
     </nav>
   );
 }
