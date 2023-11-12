0fbb70cea1c5a43d1e2da57d6f79571a2b33cb41
Justin <justinkwanchan@gmail.com>
2023-11-12 12:56:32 -0500

    Add comments

--- src/app/components/navigation/navbar/index.tsx
+++ src/app/components/navigation/navbar/index.tsx
@@ -10,18 +10,22 @@ export default function Navbar({
 }) {
   return (
     <nav className="bg-teal-700 mx-auto p-4 flex justify-between items-center">
+      {/* Left Side */}
       <div className="flex items-center gap-4">
         {/* Mobile Menu */}
         <Hamburger isOpen={isOpen} handleClick={toggleOpen} />
         <div className="bg-gray-300 w-4 h-4">{/* Montreal icon */}</div>
       </div>
-      {/* Desktop Menu */}
+
+      {/* Middle - Desktop Menu */}
       <div className="hidden md:flex justify-between md:w-2/3 lg:w-1/2">
         <Link href="/">HOME</Link>
         <Link href="/events">ALL EVENTS</Link>
         <Link href="/contribute">HOW TO CONTRIBUTE</Link>
         <Link href="/large-events">LARGE EVENTS</Link>
       </div>
+
+      {/* Right Side */}
       <div className="flex items-center gap-4">
         <div className="bg-gray-300 hidden md:block w-4 h-4">
           {/* Instagram icon */}
