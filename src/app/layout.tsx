a8c95c12b2605b64918c9fa3a2eaada522e3ecd9
Justin <justinkwanchan@gmail.com>
2023-11-12 20:13:17 -0500

    Fix layout hydration bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Navigation />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navigation />
+        {children}
+      </body>
     </html>
   );
 }
