aee2c9f09e5615d5cef4c257d5b71d63808aea7a
Justin <justinkwanchan@gmail.com>
2023-11-13 15:19:25 -0500

    Add Hero main page

---
+++ src/app/components/hero/index.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import splotch from 'public/splotch.svg';
+
+export default function Hero() {
+  return (
+    <section className="flex flex-col md:flex-row h-[calc(100vh-62px)] w-full">
+      <div className="relative w-2/5">
+        <Image
+          src={splotch}
+          alt={'Splotchy Frame'}
+          className="absolute top-8 left-8 z-10"
+        />
+        <div className="h-4/5"></div>
+        <p className="px-12">
+          Find dance classes, social events, dance studios, and performances for
+          all levels of experience in Montreal.
+        </p>
+      </div>
+      <div className="bg-black opacity-70 md:w-3/5"></div>
+    </section>
+  );
+}
