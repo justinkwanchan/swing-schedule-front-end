a288ed78ac2c979bf1257f06fbe0e3902a148c98
Justin <justinkwanchan@gmail.com>
2023-11-15 18:21:01 -0500

    Remove extra space at bottom of main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Home from './components/home';
 
 export default function Main() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex flex-col items-center justify-between">
       <Home />
     </main>
   );
