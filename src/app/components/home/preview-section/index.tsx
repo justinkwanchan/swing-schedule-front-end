f82c081aa2412954c38a45ea7e3c529c8551cb85
Justin <justinkwanchan@gmail.com>
2023-11-27 13:26:44 -0500

    Add button disabled functionality

--- src/app/components/home/preview-section/index.tsx
+++ src/app/components/home/preview-section/index.tsx
@@ -14,6 +14,8 @@ type Props = {
 export default function PreviewSection({ title }: Props) {
   const [emblaRef, emblaApi] = useEmblaCarousel();
   const [scrollProgress, setScrollProgress] = useState(0);
+  const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
 
   const onScroll = useCallback((emblaApi: EmblaCarouselType) => {
     const progress = Math.max(0, Math.min(1, emblaApi.scrollProgress()));
@@ -30,13 +32,21 @@ export default function PreviewSection({ title }: Props) {
     [emblaApi]
   );
 
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setPrevBtnDisabled(!emblaApi.canScrollPrev());
+    setNextBtnDisabled(!emblaApi.canScrollNext());
+  }, []);
+
   useEffect(() => {
     if (!emblaApi) return;
 
     onScroll(emblaApi);
+    onSelect(emblaApi);
     emblaApi.on('reInit', onScroll);
     emblaApi.on('scroll', onScroll);
-  }, [emblaApi, onScroll]);
+    emblaApi.on('reInit', onSelect);
+    emblaApi.on('select', onSelect);
+  }, [emblaApi, onScroll, onSelect]);
 
   return (
     <>
@@ -45,13 +55,28 @@ export default function PreviewSection({ title }: Props) {
           <h1 className="font-bold text-xl">{title}</h1>
           <div>
             <button
-              className={`${styles.embla__prev} mr-4`}
+              className="w-8 h-8 mr-4 disabled:opacity-30"
               onClick={scrollPrev}
+              disabled={prevBtnDisabled}
             >
-              Prev
+              <svg className="w-3/5 h-3/5" viewBox="0 0 532 532">
+                <path
+                  fill="currentColor"
+                  d="M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.796-36.208 13.796-50.002 0a5994246.277 5994246.277 0 0 0-229.332-229.454 35.065 35.065 0 0 1-10.326-25.126c0-9.2 3.393-18.26 10.326-25.2C172.192 194.973 332.731 34.31 355.66 11.354Z"
+                />
+              </svg>
             </button>
-            <button className={styles.embla__next} onClick={scrollNext}>
-              Next
+            <button
+              className="w-8 h-8 disabled:opacity-30"
+              onClick={scrollNext}
+              disabled={nextBtnDisabled}
+            >
+              <svg className="w-3/5 h-3/5" viewBox="0 0 532 532">
+                <path
+                  fill="currentColor"
+                  d="M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.796 36.208-13.796 50.002 0 22.928 22.947 206.395 206.507 229.332 229.454a35.065 35.065 0 0 1 10.326 25.126c0 9.2-3.393 18.26-10.326 25.2-45.865 45.901-206.404 206.564-229.332 229.52Z"
+                />
+              </svg>
             </button>
           </div>
         </div>
