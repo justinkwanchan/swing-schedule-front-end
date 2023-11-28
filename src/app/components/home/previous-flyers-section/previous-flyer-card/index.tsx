9658dc496447898a1b0335837b6d978a718c2e3b
Justin <justinkwanchan@gmail.com>
2023-11-28 03:39:03 -0500

    Fix PreviousFlyerCard component title

--- src/app/components/home/previous-flyers-section/previous-flyer-card/index.tsx
+++ src/app/components/home/previous-flyers-section/previous-flyer-card/index.tsx
@@ -4,7 +4,7 @@ type Props = {
   dates: string;
 };
 
-export default function DanceSchoolCard({ dates }: Props) {
+export default function PreviousFlyerCard({ dates }: Props) {
   return (
     <div className="flex flex-col flex-[0_0_100%] sm:flex-[0_0_50%] lg:flex-[0_0_33.333333%] pl-8 gap-y-1">
       <div className="bg-gray-100 w-full h-60 rounded-lg"></div>
