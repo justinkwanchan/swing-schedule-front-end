45cd871eb447809534103a377fd1c9d46ba8650c
Justin <justinkwanchan@gmail.com>
2023-11-28 03:11:27 -0500

    Fix svg width/height error

--- src/app/components/home/index.tsx
+++ src/app/components/home/index.tsx
@@ -2,11 +2,12 @@ import PreviewSection from './preview-section';
 import DanceSchoolSection from './dance-school-section';
 import PreviousFlyersSection from './previous-flyers-section';
 import PageTitleSection from '../page-title-section';
+import splotch from 'public/splotch.svg';
 
 export default function Home() {
   return (
     <>
-      <PageTitleSection splotch="public/splotch.svg" />
+      <PageTitleSection splotch={splotch} />
 
       <div className="flex flex-col w-4/5 mb-12 gap-12">
         <PreviewSection title={'Upcoming Social Dances'} />
