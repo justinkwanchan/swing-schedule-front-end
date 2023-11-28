27ce8df51803e86be0648675e587237d556d9481
Justin <justinkwanchan@gmail.com>
2023-11-28 03:38:11 -0500

    Add breakpoints to PreviousFlyerCard

--- src/app/components/home/previous-flyers-section/previous-flyer-card/index.tsx
+++ src/app/components/home/previous-flyers-section/previous-flyer-card/index.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function DanceSchoolCard({ dates }: Props) {
   return (
-    <div className="flex flex-col flex-[0_0_33.333333%] pl-8 gap-y-1">
+    <div className="flex flex-col flex-[0_0_100%] sm:flex-[0_0_50%] lg:flex-[0_0_33.333333%] pl-8 gap-y-1">
       <div className="bg-gray-100 w-full h-60 rounded-lg"></div>
       <p className="text-red-500">{dates}</p>
       <Link href="" className="underline text-cyan-400 text-sm">
