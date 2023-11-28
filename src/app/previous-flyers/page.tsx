9b4f2a567e1f192a047b65fae4cb78fc9bade06e
Justin <justinkwanchan@gmail.com>
2023-11-28 03:27:41 -0500

    Fix alignment for PreviousFlyers

--- src/app/previous-flyers/page.tsx
+++ src/app/previous-flyers/page.tsx
@@ -1,10 +1,23 @@
+import PreviousFlyerCard from '../components/home/previous-flyers-section/previous-flyer-card';
 import PageTitleSection from '../components/page-title-section';
 import splotch from 'public/splotch.svg';
 
 export default function PreviousFlyers() {
   return (
-    <>
+    <div className="flex flex-col items-center mb-12">
       <PageTitleSection splotch={splotch} />
-    </>
+
+      <div className="flex flex-wrap -ml-8 gap-y-8 w-4/5">
+        <PreviousFlyerCard dates="OCT 8 - OCT 14" />
+        <PreviousFlyerCard dates="OCT 1 - OCT 7" />
+        <PreviousFlyerCard dates="OCT 24 - OCT 30" />
+        <PreviousFlyerCard dates="OCT 8 - OCT 14" />
+        <PreviousFlyerCard dates="OCT 1 - OCT 7" />
+        <PreviousFlyerCard dates="OCT 24 - OCT 30" />
+        <PreviousFlyerCard dates="OCT 8 - OCT 14" />
+        <PreviousFlyerCard dates="OCT 1 - OCT 7" />
+        <PreviousFlyerCard dates="OCT 24 - OCT 30" />
+      </div>
+    </div>
   );
 }
